f103a90ebb33bcbc88ee5d2fc4504f92d001aa7d
juPi21 <denzybit@gmail.com>
2025-05-26 17:32:51 +0300

    new commit

--- db.js
+++ db.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb+srv://Bambinton:ex.Bambala3000@cluster0.kamvzuj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-
 const connectDB = async () => {
    try {
        await mongoose.connect(mongoURI);
@@ -10,5 +8,6 @@ const connectDB = async () => {
        console.error('Database connection failed:', err);
    }
 };
+
 
 module.exports = connectDB;
