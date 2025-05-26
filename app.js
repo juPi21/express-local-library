66b8333de4d7fb3193931cb54e38f4251a84e30a
juPi21 <denzybit@gmail.com>
2025-05-26 19:39:47 +0300

    new commit

--- app.js
+++ app.js
@@ -1,50 +1,61 @@
-require('dotenv').config()
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users.js");
+require('dotenv').config();
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const compression = require("compression");
 const helmet = require("helmet");
-const catalogRouter = require("./routes/catalog.js"); // Імпортуйте маршрути для розділу сайту "catalog"
-const compression = require("compression");
+const RateLimit = require("express-rate-limit");
 
-// Створіть об'єкт застосунку Express 
-
-const catalogRouter = require("./routes/catalog.js"); 
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users.js");
+const catalogRouter = require("./routes/catalog.js");
 const connectDB = require('./db.js');
-connectDB(); 
+
+connectDB();
 
 const app = express();
 
-const RateLimit = require("express-rate-limit");
+// Обмеження запитів
 const limiter = RateLimit({
   windowMs: 1 * 60 * 1000, // 1 хвилина
   max: 20,
 });
-// Застосуйте обмеження швидкості до всіх запитів ds
 app.use(limiter);
+
+// Helmet (з політикою безпеки)
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  })
+);
+
+// Налаштування view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(compression()); // Стисніть усі маршрути
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Маршрути
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/catalog", catalogRouter);
 
+// 404
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
+// Обробник помилок
 app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -52,17 +63,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// Запуск сервера
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
-
-app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
-    },
-  }),
-);
-
 
 module.exports = app;
